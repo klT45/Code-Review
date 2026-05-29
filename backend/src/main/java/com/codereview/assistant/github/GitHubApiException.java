c5ea2ad099561afa6429081358553c160a03e759
Your Name <3131953998@qq.com>
2026-05-29 10:00:11 +0800

    获取PR基础信息

---
+++ backend/src/main/java/com/codereview/assistant/github/GitHubApiException.java
@@ -0,0 +1,12 @@
+package com.codereview.assistant.github;
+
+public class GitHubApiException extends RuntimeException {
+
+    public GitHubApiException(String message) {
+        super(message);
+    }
+
+    public GitHubApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
